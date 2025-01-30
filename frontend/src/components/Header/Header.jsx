07f2bf1fe2b3837e8f2f063f7998cf3f0e43be7f
Ohang <ohangpatel12@gmail.com>
2025-01-29 22:18:43 -0600

    Fix submodule issue

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <div className="header-contents">
+            <h2>Order your favourite food here</h2>
+            <p>Choose your diverse menu featuring a delectable array of dishes crefted with the finest     Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+            <button>View Menu</button>
+        </div>
+    </div>
+  )
+}
+
+export default Header
